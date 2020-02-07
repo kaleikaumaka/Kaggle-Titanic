df73d918f4107216b19c77c03a10f1308fd877d9
kaleikaumaka <alexander.k.mathews@gmail.com>
2020-02-06 19:34:02 -0500

    added venv and started working

---
+++ main.py
@@ -0,0 +1,7 @@
+import numpy as np #linear algebra
+import pandas as pd #data manipulation
+import seaborn as sb #visualization containing matplotlib
+import bokeh #interactive visualization
+import os #operating system filing
+import json #markup
+
